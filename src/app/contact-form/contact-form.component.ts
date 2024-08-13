e63bb29b17f01e3dcdd48dbbae84f4b6e6acccb8
Carlos Miguel Rodriguez Botero <carlos.rodriguez1@utp.edu.co>
2024-08-13 13:59:05 -0500

    imporved user view and error control, display of errors in alert

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ContactService } from '../services/contact.service';
 import { Contact } from '../model/contact.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contact-form',
@@ -11,7 +12,7 @@ import { Contact } from '../model/contact.interface';
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
-export default class ContactFormComponent implements OnInit{
+export default class ContactFormComponent implements OnInit {
 
   private fb = inject(FormBuilder);
   private router = inject(Router);
@@ -20,39 +21,52 @@ export default class ContactFormComponent implements OnInit{
 
   form?: FormGroup;
   contact?: Contact;
+  errors: string[] = [];
 
   ngOnInit(): void {
-      const id = this.route.snapshot.paramMap.get('id');
+    const id = this.route.snapshot.paramMap.get('id');
 
-      if(id){
-        this.contactService.get(parseInt(id)).subscribe( contact => {
-          this.contact = contact;
-          this.form = this.fb.group({
-            name: [contact.name, [Validators.required]],
-            email: [contact.email, [Validators.required, Validators.email]]
-          });
-        })  
-      }else{
+    if (id) {
+      this.contactService.get(parseInt(id)).subscribe(contact => {
+        this.contact = contact;
         this.form = this.fb.group({
-          name: ['', [Validators.required]],
-          email: ['', [Validators.required, Validators.email]]
+          name: [contact.name, [Validators.required]],
+          email: [contact.email, [Validators.required, Validators.email]]
         });
-      }
+      })
+    } else {
+      this.form = this.fb.group({
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]]
+        /* name: ['', []],
+        email: ['', []] */
+      });
+    }
   }
 
   save() {
-    if(this.form?.invalid){
+    if (this.form?.invalid) {
+      this.form.markAllAsTouched();
       return;
     }
     const contactForm = this.form!.value;
+    let request: Observable<Contact>;
+
     if (this.contact) {
-      this.contactService.update(this.contact.id, contactForm).subscribe(() => {
-        this.router.navigate(['/']);
-      });
+      request = this.contactService.update(this.contact.id, contactForm);
     } else {
-      this.contactService.create(contactForm).subscribe(() => {
-        this.router.navigate(['/']);
-      });
+      request = this.contactService.create(contactForm);
     }
+
+    request.subscribe({
+      next: () => {
+        this.errors = [];
+        this.router.navigate(['/']);
+      },
+      error: response => {
+        this.errors = response.error.errors;
+      }
+    });
   }
+
 }
